39502d14b79a2424e8acfb585ef83ff511738807
RathiAbhi <34103954+RathiAbhi@users.noreply.github.com>
2019-07-25 18:32:45 +0530

    initial commit

---
+++ Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private object lbstatus;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString["timeout"] != null)
+        {
+            //lbstatus.Text = "You test time expired, please try again!";
+        }
+    }
+}
+
